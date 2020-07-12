70cb42db4149592bcde8b4cc199fce6364df2663
MrFlynn <nick@pleatsikas.me>
2020-07-11 20:39:48 -0700

    Bumped release version in Homebrew formula.

--- HomebrewFormula/git-absorb.rb
+++ HomebrewFormula/git-absorb.rb
@@ -1,9 +1,9 @@
 class GitAbsorb < Formula
-  version "0.2.0"
+  version "0.2.1"
   desc "An extension for Git that combines multiple repositories into a single repository"
   homepage "https://github.com/MrFlynn/git-absorb"
-  url "https://github.com/MrFlynn/git-absorb/archive/v0.2.0.tar.gz"
-  sha256 "9e87daac2b6cb182e02657fdb8ff085ad018cf6164919ac21493a355fe4291c6"
+  url "https://github.com/MrFlynn/git-absorb/archive/v0.2.1.tar.gz"
+  sha256 "02f2b8370858f347975ff2525f9fca478b2ae3a92e8a1d7be4393bb8d854c545"
 
   depends_on "bash"
 
